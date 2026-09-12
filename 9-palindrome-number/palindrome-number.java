c560c56986d929ba81159464b96467544ecb06c1
Aarish khan <khan.aarish2346@gmail.com>
2026-09-12 13:58:33 +0530

    Time: 6 ms (22.11%) | Memory: 45.7 MB (86.67%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original=x;
+        int ans=0;
+        if(x<0){
+            return false;
+        }
+        
+        while(x!=0){
+         
+            int digit=x%10;
+            ans=(ans*10)+digit;
+            x=x/10;
+        //     if (ans > (Integer.MAX_VALUE - digit) / 10) {
+        //     return false;
+        // }
+        }
+        
+        
+            return (ans==original);
+        
+
+
+
+        
+    }
+}
